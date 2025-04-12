e3e459418e806a9ffef951313ec108c9b784b5a9
joseerickpsantos@gmail.com <erickjosepinheiro09@gmail.com>
2025-04-12 17:27:01 -0300

    coloquei uma condição basica

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 print("Olá, primeiro projeto")
 print("Não funcionou")
+print("Funcionou!!")
+idade = int(input("Qual sua idade>>"))
+if idade >= 18 :
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
